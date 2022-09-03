cf5ccee49a285a7e1f2068b30bb3e75b50411fa3
kartik050503 <100618357+kartik050503@users.noreply.github.com>
2022-09-03 16:10:34 +0530

    Add files via upload

---
+++ StarPattern.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int trows;
+    cin>>trows;
+    int rowno=1;
+
+    while(rowno<=trows){
+         
+    int countstar=1;
+    while(countstar<=rowno){
+        cout<<"*"<<" ";
+        countstar=countstar+1;
+
+    }
+    cout<<endl;
+    rowno=rowno+1;
+    }
+
+}
