916dc999ac21e79057d4dcbec10432d466a2312c
kartik050503 <100618357+kartik050503@users.noreply.github.com>
2022-09-11 18:19:44 +0530

    Add files via upload

---
+++ SetBits.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int num;
+    cin>>num;
+    int count=0;
+    while(num>0)
+    {
+        if((num&1)==1)
+        {
+            count++;
+
+        }
+        num=num>>1;
+    }
+    cout<<count;
+    return 0;
+   
+}
